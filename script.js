8c552852861bd4a9d7931b21f3af0aa15b37e96d
mack66hm <mack66hm@gmail.com>
2020-01-30 19:27:37 -0500

    get invalid/valid color

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log('Add validation!');
+// console.log('Add validation!');
+
+// let gotIT = document.createElement('class')
+// let 
+let parkingForm = document.querySelector('#parking-form')
+let submitButton = document.querySelector('#submit-button')
+let inputFields = document.querySelectorAll('.field')
+//    let box = document.querySelector('#name-field')
+//     box.classList.add('input-invalid')
+    
+//     for (let filed of inputFilds){
+
+//     }
+
+
+parkingForm.addEventListener('submit', function(e){
+   e.preventDefault();
+    for (let field of inputFields){
+        if (field.value.length > 0) {
+            field.parentElement.classList.add('input-valid')
+        }
+        else {
+            field.parentElement.classList.add('input-invalid')
+        }
+    }
+})
+
+
